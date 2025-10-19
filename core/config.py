4b9ce766aaa07b07d1fe2de44c4da277f604247b
NghiaDT <nghiadt22.work@gmail.com>
2025-10-19 10:12:42 +0700

    update

--- core/config.py
+++ core/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 DISCORD_GUILD_ID = os.getenv("DISCORD_GUILD_ID")
 FFMPEG_PATH = os.getenv("FFMPEG_PATH", "ffmpeg")
+HOST_URL=os.getenv("HOST_URL", f"http://localhost:8000")
 
 OUTPUT_DIR="downloads"
 OUTPUT_URL="export"
-HOST_URL=f"http://localhost:8000"
